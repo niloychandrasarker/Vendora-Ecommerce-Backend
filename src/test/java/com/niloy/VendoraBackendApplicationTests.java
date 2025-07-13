9a3156c63ec6b9ecdc8d98472897afe22585fcc3
niloychandrasarker <niloysarker.cs@gmail.com>
2025-07-13 18:11:12 +0600

    Initial setup {intigrate dependency & configure application.properties

---
+++ src/test/java/com/niloy/VendoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niloy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
